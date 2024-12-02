52e66368643a570687e7f9f9b600a87b2ab383f4
Pedro Castro <falecompedroac@gmail.com>
2024-12-02 21:22:12 +0000

    elections 17

---
+++ elections/17.sql
@@ -0,0 +1,20 @@
+-- Was there any party winning on all parishes of any municipality of the district of Porto?
+
+-- List the code and name of the municipality and the acronym of the party.
+
+-- Order ascendantly by the code of the municipality and the acronym of the party.
+
+SELECT M.code, M.name, V.party AS PARTY
+FROM votings V, parishes P, municipalities M, districts D
+WHERE V.parish = P.code AND P.municipality = M.code AND M.district = D.code AND D.name = 'Porto' AND V.votes = (
+    SELECT MAX(V1.votes)
+    FROM votings V1
+    WHERE V1.parish = P.code
+)
+GROUP BY M.code, M.name, V.party
+HAVING COUNT(DISTINCT P.code) = (
+    SELECT COUNT(P2.code)
+    FROM parishes P2
+    WHERE P2.municipality = M.code
+)
+ORDER BY M.code, V.party
