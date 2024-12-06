37110b3f3763f4d5f207203623c6b43aff16d65e
Pedro Castro <falecompedroac@gmail.com>
2024-12-06 22:26:03 +0000

    elections 05-07 using JOIN

--- elections/05.sql
+++ elections/05.sql
@@ -1,6 +1,9 @@
 -- How many seats obtained each party on the district of the parish of Mancelos, in decreasing order of the number of seats? List the acronym and the number of seats.
 
 SELECT L.party, L.seats
-FROM parishes P, municipalities M, districts D, lists L
-WHERE P.municipality = M.code AND M.district = D.code AND L.district = D.code AND P.name='Mancelos'
+FROM parishes P
+JOIN municipalities M on M.code = P.municipality
+JOIN districts D ON D.code = M.district
+JOIN lists L on L.district = D.code
+WHERE P.name='Mancelos'
 ORDER BY L.seats DESC;
--- elections/07.sql
+++ elections/07.sql
@@ -1,6 +1,9 @@
 -- What are the parishes of region 'M' where the party 'PSN' obtained zero votes? List the code and name of the parish, in ascending order on the code.
 
 SELECT DISTINCT P.code, P.name
-FROM parishes P, municipalities M, districts D, votings V
-WHERE P.municipality = M.code AND M.district = D.code AND D.region = 'M' AND V.parish = P.code AND V.party = 'PSN' AND V.votes = 0
+FROM parishes P
+JOIN municipalities M ON M.code = P.municipality
+JOIN districts D ON D.code = M.district
+JOIN votings V ON V.parish = P.code
+WHERE D.region = 'M' AND V.party = 'PSN' AND V.votes = 0
 ORDER BY P.code ASC;
