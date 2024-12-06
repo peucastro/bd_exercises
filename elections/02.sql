e4f16e972f9f1fd42583de2f20775c5a9b4fa1ab
Pedro Castro <falecompedroac@gmail.com>
2024-12-06 16:03:03 +0000

    elections 02-04 using JOIN

--- elections/02.sql
+++ elections/02.sql
@@ -1,6 +1,7 @@
 -- What were the results obtained by each party in the parish of Mancelos? List the acronym of each party and the number of votes it got, descending on the number of votes and ascendingly by acronym.
 
 SELECT V.party, V.votes
-FROM parishes P, votings V
-WHERE P.code = V.parish AND P.name='Mancelos'
+FROM parishes P
+JOIN votings V ON V.parish = P.code
+WHERE P.name='Mancelos'
 ORDER BY P.code ASC, V.votes DESC;
--- elections/03.sql
+++ elections/03.sql
@@ -1,6 +1,7 @@
 -- What are the parishes of the municipality of Espinho? List the code and name of each parish, order ascendantly by the code.
 
 SELECT P.code, P.name
-FROM municipalities M, parishes P
-WHERE M.code = p.municipality AND M.name='Espinho'
+FROM municipalities M
+JOIN parishes P ON P.municipality = M.code
+WHERE M.name='Espinho'
 ORDER BY P.code ASC;
