4388f7233699429ba9490b5df200901cf5feb990
Pedro Castro <falecompedroac@gmail.com>
2024-12-06 22:41:35 +0000

    elections 14-14 using JOIN

--- elections/14.sql
+++ elections/14.sql
@@ -3,5 +3,7 @@
 --List the acronym of the party, the name of the parish and the number of votes it obtained in that parish.
 
 SELECT PTS.acronym AS PARTY, PSH.name, V.votes
-FROM votings V, parishes PSH, parties PTS
-WHERE V.parish = PSH.code AND V.party = PTS.acronym AND NOT EXISTS (SELECT * FROM votings V1 WHERE V1.votes > V.votes);
+FROM votings V
+JOIN parishes PSH ON PSH.code = V.parish
+JOIN parties PTS ON PTS.acronym = V.party
+WHERE NOT EXISTS (SELECT * FROM votings V1 WHERE V1.votes > V.votes);
--- elections/15.sql
+++ elections/15.sql
@@ -3,8 +3,10 @@
 -- Order ascendantly by acronym.
 
 SELECT P.acronym AS PARTY
-FROM lists L, districts D, parties P
-WHERE L.district = D.code AND L.party = P.acronym AND (D.name LIKE '%o%' OR D.name LIKE '%O%')
+FROM lists L
+JOIN districts D ON D.code = L.district
+JOIN parties P ON P.acronym = L.party
+WHERE D.name LIKE '%o%' OR D.name LIKE '%O%'
 GROUP BY P.acronym
 HAVING COUNT(L.district) = (
     SELECT COUNT(D1.code)
