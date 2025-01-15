40c57aa8c4ac395400bc5ff9238607cf11aace69
Pedro Castro <falecompedroac@gmail.com>
2025-01-15 15:45:48 +0000

    elections: simplified 12

--- elections/12.sql
+++ elections/12.sql
@@ -4,8 +4,7 @@
 
 -- List the acronym of the party and the total number of votes, name VOTES. Order ascendantly by the acronym.
 
-SELECT p.acronym AS PARTY, SUM(V.votes) AS VOTES
-FROM votings V
-JOIN parties P ON P.acronym = V.party
-GROUP BY P.acronym
-ORDER BY P.acronym
+SELECT party, SUM(votes) AS VOTES
+FROM votings
+GROUP BY party
+ORDER BY party ASC;
