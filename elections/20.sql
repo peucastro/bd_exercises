3b414550f5d45472a6e5f85b085cc0a5d2dcd8d8
Pedro Castro <falecompedroac@gmail.com>
2025-01-15 20:52:02 +0000

    elections: improved 20

--- elections/20.sql
+++ elections/20.sql
@@ -2,9 +2,10 @@
 
 -- Order ascendantly by the acronym and the name of the municipality.
 
-SELECT PTS.acronym AS PARTY, M.name AS NAME
-FROM votings V, parishes PSH, municipalities M, parties PTS
-WHERE V.parish = PSH.code AND V.party = PTS.acronym AND PSH.municipality = M.code
-GROUP BY PTS.acronym, M.code
+SELECT V.party AS PARTY, M.name AS NAME
+FROM votings V
+JOIN parishes P ON P.code = V.parish
+JOIN municipalities M ON M.code = P.municipality
+GROUP BY PARTY, M.code
 HAVING SUM(V.votes) = 0
 ORDER BY PARTY, NAME
