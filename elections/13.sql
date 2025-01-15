c5dc703a79aac7df0c2b9c47e6f298aaf460f04a
Pedro Castro <falecompedroac@gmail.com>
2025-01-15 15:48:52 +0000

    elections: optimized 13

--- elections/13.sql
+++ elections/13.sql
@@ -2,9 +2,12 @@
 
 -- Order ascendantly by the name of the municipality.
 
-SELECT DISTINCT M.name
-FROM participations P
-JOIN districts D ON D.code = P.district
-JOIN municipalities M ON M.district = D.code
-WHERE NOT EXISTS (SELECT * FROM participations P1 WHERE P1.voters > P.voters)
-ORDER BY M.name;
+SELECT name
+FROM municipalities
+WHERE district = (
+    SELECT district
+    FROM participations
+    ORDER BY voters DESC
+    LIMIT 1
+)
+ORDER BY name;
