00b6b8c420193cacac70ff8f4700fdafd3b604d5
Pedro Castro <falecompedroac@gmail.com>
2025-01-20 17:56:34 +0000

    exams/23_24-v2: simplified 05

--- exams/23_24-v2/05.sql
+++ exams/23_24-v2/05.sql
@@ -2,25 +2,14 @@
 
 -- List the the name of the municipality (MNAME), the name of the district (DNAME) and the total number of parishes (TOTAL). Order on the name of the municipality.
 
-DROP VIEW IF EXISTS MUN_PAR_COUNT;
-DROP VIEW IF EXISTS DIST_MUN_COUNT;
-
-CREATE VIEW MUN_PAR_COUNT AS
-SELECT M.code as mun, COUNT(*) AS pcount
-FROM municipalities M
-JOIN parishes P ON P.municipality = M.code
-GROUP BY M.code;
-
-CREATE VIEW DIST_MUN_COUNT AS
-SELECT D.code as dist, COUNT(*) AS mcount
-FROM districts D
-JOIN municipalities M ON M.district = D.code
-GROUP BY D.code;
-
-SELECT M.name AS MNAME, D.name AS DNAME, pcount AS TOTAL
-FROM municipalities M
+SELECT M.name AS MNAME, D.name AS DNAME, COUNT(P.code) AS TOTAL
+FROM parishes P
+JOIN municipalities M ON M.code = P.municipality
 JOIN districts D ON D.code = M.district
-JOIN MUN_PAR_COUNT MPC ON MPC.mun = M.code
-JOIN DIST_MUN_COUNT DMC ON DMC.dist = M.district
-WHERE pcount > 20 AND pcount = mcount
+GROUP BY M.code
+HAVING TOTAL > 20 AND TOTAL = (
+    SELECT COUNT(M1.code)
+    FROM municipalities M1
+    WHERE M1.district = M.district
+)
 ORDER BY MNAME;
