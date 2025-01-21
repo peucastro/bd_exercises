61a3203e87685ffc0043ace62ca48a14df1d2a97
Pedro Castro <falecompedroac@gmail.com>
2025-01-21 14:52:00 +0000

    exams/23_24-v2: simplified 06

--- exams/23_24-v2/06.sql
+++ exams/23_24-v2/06.sql
@@ -1,7 +1,6 @@
 -- Select the acronyms of all parties and the total number of parishes (as NPARISHES) where they obtained more than 1000 votes. Order descendently on NPARISHES.
 
-SELECT PTS.acronym, SUM(V.votes > 1000) AS NPARISHES
-FROM parties PTS
-JOIN votings V ON V.party = PTS.acronym
+SELECT party, SUM(votes > 1000) AS NPARISHES
+FRMO votings
 GROUP BY V.party
-ORDER BY NPARISHES DESC;
+ORDER BY NPARISHES DESC, party;
